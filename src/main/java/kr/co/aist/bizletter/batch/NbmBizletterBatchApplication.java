7a25bd4621f0ecb25626c79b4c4458ac498681a3
udonguk <udonguk@gmail.com>
2021-10-22 17:23:38 +0900

    first commit

---
+++ src/main/java/kr/co/aist/bizletter/batch/NbmBizletterBatchApplication.java
@@ -0,0 +1,13 @@
+package kr.co.aist.bizletter.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbmBizletterBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbmBizletterBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/aist/bizletter/batch/NbmBizletterBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.aist.bizletter.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbmBizletterBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
